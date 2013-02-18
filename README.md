1f170f68100ec54d87f89c89c9db4d2bab1035e6
Kingston Tam <web@kingstontam.com>
2013-02-17 21:13:40 -0800

    First attempt at git ready script

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-rapidenv
+GitReady
 ========
 
-A one-line setup script to get the basics of a server up and running.
+A one-line setup script to get it github ready
+
+*NOTE: Currently only for Ubuntu*
+
+Linux
+-----
+`wget https://raw.github.com/kingston/gitready/master/gitready.sh && bash gitready.sh`
+
+OS X
+----
+`curl -O https://raw.github.com/kingston/gitready/master/gitready.sh && bash gitready.sh`
+
+What does it do?
+----------------
+
+   * Installs Git
+   * Generates an ssh key
+   * Provides you a Git key to set up
---
+++ gitready.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#
+# Git Ready
+#
+# A very simple script to get git set-up along with the necessary keys
+#
+# Inspiration: https://github.com/joshfng/railsready
+#
+
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+
+control_c()
+{
+  echo -en "\n\n*** Exiting ***\n\n"
+  exit 1
+}
+
+USER_HOME=$(eval echo ~${SUDO_USER})
+
+# trap keyboard interrupt (control-c)
+trap control_c SIGINT
+
+if command -v aptitude >/dev/null 2>&1 ; then
+  pm="aptitude"
+else
+  pm="apt-get"
+fi
+
+if [ ! -f "$USER_HOME/.ssh/id_rsa" ]; then
+    echo "Setting up SSH keys..."
+    read -p "Please enter the email address to associate with it: " email
+    ssh-keygen -t rsa -C $email -f $USER_HOME/.ssh/id_rsa
+    echo "SSH keys generated!"
+else
+    echo "SSH keys found, skipping step!"
+fi
+
+echo "Installing git..."
+
+sudo $pm -y install git-core
+
+echo "done!\n\n"
+
+echo "Go to https://github.com/settings/ssh and add the following key:"
+
+cat ~/.ssh/id_rsa.pub
